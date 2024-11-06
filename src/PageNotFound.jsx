3107ac7a8089c14d7a0c7c1e3dc7e136a0f68258
srihitha <vanjarapu.srihitha999@gmail.com>
2024-11-06 20:00:12 +0530

    commit8

--- src/PageNotFound.jsx
+++ src/PageNotFound.jsx
@@ -1,15 +1,20 @@
-import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./PageNotFound.css"; // Import CSS file for styling
 
 function PageNotFound() {
-  const navigate = useNavigate();
-
-  // Automatically navigate to the homepage when the component mounts
-  useEffect(() => {
-    navigate("/HomePage");
-  }, [navigate]);
-
-  return null; // Don't display anything
+  return (
+    <div className="not-found-container">
+      <h2 className="not-found-heading">404 - Page Not Found</h2>
+      <p className="not-found-message">
+        Oops! The page you are looking for could not be found.
+      </p>
+      <p className="not-found-suggestion">
+        Please check the URL or go back to the{" "}
+        <Link to="HomePage">homepage</Link>.
+      </p>
+    </div>
+  );
 }
 
 export default PageNotFound;
